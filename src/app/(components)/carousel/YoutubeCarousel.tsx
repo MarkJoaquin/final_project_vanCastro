1c5e10073240ec1a0042aea052240bc8dc9cd520
Sho8000 <yoshikichi8000@gmail.com>
2025-03-05 18:45:21 -0800

    fixed Youtube Carousel

--- src/app/(components)/carousel/YoutubeCarousel.tsx
+++ src/app/(components)/carousel/YoutubeCarousel.tsx
@@ -4,12 +4,10 @@ export default function YoutubeCarousel() {
 
   return (
     <>
-      <div className="tagembed-widget" style={{width:"80%",height:"100%",margin:"auto"}} data-widget-id="2157471" data-tags="false"  view-url="https://widget.tagembed.com/2157471"></div>
+      <div className="w-[80%] h-[350px] overflow-y-scroll m-auto">
+        <div className="tagembed-widget" style={{width:"100%",height:"100%",margin:"auto"}} data-widget-id="2157471" data-tags="false"  view-url="https://widget.tagembed.com/2157471"></div>
+      </div>
       <Script src="https://widget.tagembed.com/embed.min.js" type="text/javascript"/>
     </>
   );
 }
-
-/* 
-<div class="tagembed-widget" style="width:80%;height:100%" data-widget-id="2157471" data-tags="false"  view-url="https://widget.tagembed.com/2157471"></div><script src="https://widget.tagembed.com/embed.min.js" type="text/javascript"></script>
-*/
