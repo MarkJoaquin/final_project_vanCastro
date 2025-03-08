f1f5829099d8dd251d329444a5ca69bb16f29dde
Sho8000 <yoshikichi8000@gmail.com>
2025-03-08 13:20:32 -0800

    created review section

--- src/app/Review/Review.tsx
+++ src/app/Review/Review.tsx
@@ -1,14 +1,37 @@
+"use client"
 import SelectorBtn from "../(components)/Button/SelectorBtn";
+import { useSelectBtnContext } from "../(context)/SelectBtnContext";
+import AlumniCarousel from "../(components)/Carousels/AlumniCarousel";
+import GoogleCarousel from "../(components)/Carousels/GoogleCarousel";
+import YoutubeCarousel from "../(components)/Carousels/YoutubeCarousel";
 
 interface selectBtnProps {
   btnName:string[]
 }
 
 export default function Review({btnName}:selectBtnProps){
-    return(
-      <section>
-        <h2>Check Out Real Reviews</h2>
+  const {activeBtn} = useSelectBtnContext();
+
+  const setReview = () => {
+    if(activeBtn==="Google Review"){
+      return <GoogleCarousel/>
+    }
+    if(activeBtn==="Video Review"){
+      return <YoutubeCarousel/>
+    }
+    //Default
+    return <AlumniCarousel/>
+  }
+  
+  return(
+    <section>
+      <h2 className="text-[50px] font-[700] text-center p-[30px]">Check Out Real Reviews</h2>
+      <div className="pb-4">
         <SelectorBtn btnName={btnName}/>
-      </section>
-    )
+      </div>
+      <div className="pt-4 pb-4">
+        {setReview()}
+      </div>
+    </section>
+  )
 }
