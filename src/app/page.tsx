25e11239d918be779e9772be2134629dc1412c2c
ivanmatey <ivanmatey5@gmail.com>
2025-03-13 20:44:34 -0700

    fixed styles again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,6 @@ export default function Home() {
       {/* <AlumniCarousel/>
       <GoogleCarousel/>
       <YoutubeCarousel/> */}
-      <Steps data = {steps as StepsSection[]} />
       <PickAPlan/>
     </>
   );
