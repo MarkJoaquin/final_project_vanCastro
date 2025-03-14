0ee9006fbb59400f438681e49f250b0cea07f456
MarkJoaquin <markjoaquin2@gmail.com>
2025-03-13 18:42:48 -0700

    done

--- src/app/(components)/Carousels/AlumniCarousel.tsx
+++ src/app/(components)/Carousels/AlumniCarousel.tsx
@@ -1,16 +1,23 @@
-"use client"
+"use client";
 import Script from "next/script";
 
 export default function AlumniCarousel() {
-
   return (
     <>
       <div className="pr-[20px] pl-[20px] h-[auto]">
-        <div className="senja-embed" data-id="3399a6a4-22ca-4f9d-a078-a3b178e87828" data-mode="shadow" data-lazyload="true" style={{display:"block", minHeight:"fit-content"}}></div>
-
-        <Script src="https://widget.senja.io/widget/3399a6a4-22ca-4f9d-a078-a3b178e87828/platform.js" type="text/javascript" async />
+        <Script
+          src="https://widget.senja.io/widget/3399a6a4-22ca-4f9d-a078-a3b178e87828/platform.js"
+          type="text/javascript"
+          async
+        />
+        <div
+          className="senja-embed"
+          data-id="3399a6a4-22ca-4f9d-a078-a3b178e87828"
+          data-mode="shadow"
+          data-lazyload="false" 
+          style={{ display: "block", width: "100%", height: "auto" }} 
+        ></div>
       </div>
     </>
   );
 }
-
