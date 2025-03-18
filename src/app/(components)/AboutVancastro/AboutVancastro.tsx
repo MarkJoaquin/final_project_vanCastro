adc4fcd69d6a9c390322fd4bbce47501facca378
Kevin Patino <kevinpatcastrejon95@gmail.com>
2025-03-17 19:52:53 -0700

    made styles from about vancastro

--- src/app/(components)/AboutVancastro/AboutVancastro.tsx
+++ src/app/(components)/AboutVancastro/AboutVancastro.tsx
@@ -1,3 +1,5 @@
+import styles from './AboutVancastro.module.css';
+
 type Props = {
     title: string;
     subtitle: string;
@@ -11,8 +13,9 @@ export default function AboutVancastro({data}: AboutVancastroProps) {
     const { title, subtitle } = data;
 
     return (
-        <div className="aboutVancastro">
-            
+        <div className={styles.aboutVancastro}>
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.subtitle}>{subtitle}</p>
         </div>
     )
 }
