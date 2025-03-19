4a84ec27c3d7b975ae2c00e43b6e63fde9305625
Sho8000 <yoshikichi8000@gmail.com>
2025-03-18 19:52:11 -0700

    save

---
+++ src/app/(components)/NeedHelpICBC/NeedHelpICBC.tsx
@@ -0,0 +1,12 @@
+
+export default function NeedHelpICBC() {
+//    const { title1, title2, subtitle, title1Color, title2Color, subtitleColor, img1, img2, buttonText, linkTo, bgColor, btnTextColor, type, btnColor } = data;
+
+
+    return (
+        <section>
+          <div></div>
+          <div>card</div>
+        </section>
+    );
+}
--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,7 +1,26 @@
+import NeedHelpICBC from "../(components)/NeedHelpICBC/NeedHelpICBC";
+
+const NeedHelpICBCdata = [
+    {
+        title1:"Need help with the",
+        title2:"ICBC Knowledge Test?",
+        subTitle:`Connect with Our Partner!`,
+        cardInfo:[
+            {
+                phoneIcon:"",
+                phoneNumber:"+1 (236)-513-2741",
+                email:"icbcknowledgetestmaterial@gmail.com",
+                socialMediaIcon:["",""]
+            }
+        ],
+    }
+]
+
 export default function Booking() {
     return (
-        <div>
+        <div className="min-h-[300px] pt-[300px]">
             <h1>This is the booking page</h1>
+            <NeedHelpICBC/>
         </div>
     )
 }
