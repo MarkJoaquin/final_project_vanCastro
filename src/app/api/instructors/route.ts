c30ab5852b1468adbd2b78d91c573555d0aa641b
Sho8000 <yoshikichi8000@gmail.com>
2025-03-28 00:24:01 -0700

    added all endpoints for instructors

--- src/app/api/instructors/route.ts
+++ src/app/api/instructors/route.ts
@@ -18,18 +18,6 @@ export async function POST(request: NextRequest) {
     if (!body.email || !body.name || !body.languages || !body.phone || !body.password) {
       return NextResponse.json({ message: "Missing information" });
     }
-/* 
-    model Instructor {
-      id             String           @id @default(auto()) @map("_id") @db.ObjectId
-      name           String
-      languages      String[]
-      phone          String
-      email          String           @unique
-      password       String
-      licenseNumber  String?
-      experienceYears Int?
-    }
- */
     const newInstructor = await prisma.instructor.create({
       data: {
         email: body.email as string,
@@ -49,5 +37,44 @@ export async function POST(request: NextRequest) {
   } catch (error) {
     console.error("Error adding new Instructor Info:", error);
     return NextResponse.json({ success: false, error: "Failed to add Instructor Info" });
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
+  const body = await request.json();
+
+  console.log(id)
+
+  if( !id ){
+    return NextResponse.json(
+      { status: "error", message: "Instructor ID is required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const updateInstructor = await prisma.instructor.update({
+      where:{
+        id: id,
+      },
+      data:{
+        name: body.name,
+        languages: body.languages,
+        phone: body.phone,
+        email: body.email,
+        password: body.password,
+        ...(body.licenseNumber && { licenseNumber: body.licenseNumber as string }),
+        ...(body.experienceYears && {experienceYears:body.experienceYears as number})
+      }
+    });
+
+    return NextResponse.json({
+      status: "success",
+      data: updateInstructor,
+    });
+  } catch (error) {
+    console.error("Error updating Instructor Info:", error);
+    return NextResponse.json({ success: false, error: "Failed to update Instructor Info" });
   }
 }
