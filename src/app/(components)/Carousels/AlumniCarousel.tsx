9e35334e16c234e7338585a509da81cb450b1d78
Sho8000 <yoshikichi8000@gmail.com>
2025-04-28 14:10:11 -0700

    fixed carousel

--- src/app/(components)/Carousels/AlumniCarousel.tsx
+++ src/app/(components)/Carousels/AlumniCarousel.tsx
@@ -4,20 +4,18 @@ import Script from "next/script";
 export default function AlumniCarousel() {
   return (
     <>
-      <div className="pr-[20px] pl-[20px] h-[auto]">
-        <Script
-          src="https://widget.senja.io/widget/3399a6a4-22ca-4f9d-a078-a3b178e87828/platform.js"
-          type="text/javascript"
-          async
-        />
-        <div
-          className="senja-embed"
-          data-id="3399a6a4-22ca-4f9d-a078-a3b178e87828"
-          data-mode="shadow"
-          data-lazyload="false" 
-          style={{ display: "block", width: "100%", height: "auto" }} 
-        ></div>
-      </div>
+      <Script
+        src="https://widget.senja.io/widget/a537765a-2cf2-450c-8e6b-cd60b4139d1d/platform.js"
+        strategy="afterInteractive" // IMPORTANT: ensures proper loading timing
+      />
+      <div
+        className="senja-embed"
+        data-id="a537765a-2cf2-450c-8e6b-cd60b4139d1d"
+        data-mode="shadow"
+        data-lazyload="false"
+        style={{ width: "100%", height: "auto" }}
+      ></div>
     </>
   );
+
 }
